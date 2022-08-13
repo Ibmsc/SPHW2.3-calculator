fa05f727ec75cbe62cca4a309e3b7922c7ad7106
ibmsc <Ibm.sc1409@gmail.com>
2022-08-13 12:38:09 +0300

    2 commit

---
+++ src/main/java/CalculateAplication/service/CalculateService.java
@@ -0,0 +1,20 @@
+package CalculateAplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculateService {
+
+    public int plus(Integer a, Integer b) {
+        return a + b;
+    }
+    public int minus(Integer a, Integer b) {
+        return a - b;
+    }
+    public int multiply(Integer a, Integer b) {
+        return a * b;
+    }
+    public double divide(Integer a, Integer b){
+    return a / b;
+    }
+}
