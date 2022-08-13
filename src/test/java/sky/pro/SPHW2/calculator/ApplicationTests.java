1cc2ea215412731e2b4ffd3c86f8c0babb868f21
ibmsc <Ibm.sc1409@gmail.com>
2022-08-13 11:02:56 +0300

    Initial commit

---
+++ src/test/java/sky/pro/SPHW2/calculator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.SPHW2.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
